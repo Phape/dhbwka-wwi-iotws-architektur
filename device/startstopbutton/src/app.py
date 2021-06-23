6f7699918d65661b344eec29c38db09f86de532b
Philipp Mayer <philipp.mayer@phape.de>
2021-06-23 12:04:03 +0200

    cleanup, add functionality to deactivate alarm, improve logging

--- device/startstopbutton/src/app.py
+++ device/startstopbutton/src/app.py
@@ -3,7 +3,8 @@
 import redis, RPi.GPIO as GPIO
 import configparser, logging, os, time, sys
 
-REDIS_KEY_MEASUREMENT_ENABLED = "measurement:enabled"
+REDIS_ALERT_SYSTEM_ACTIVE = "system:active"
+REDIS_ALERT_ENABLED = "alert:enabled"
 
 class App:
     """
@@ -49,11 +50,6 @@ class App:
         self._logger.info("Stelle Verbindung zu Redis her: host=%(host)s, port=%(port)s, db=%(db)s" % redis_config)
         self._redis = redis.Redis(decode_responses=True, **redis_config)
 
-
-        pull_up_down  = os.getenv("BUTTON_PULL_UP_DOWN")  or self._config["button"]["pull_up_down"]
-        event         = os.getenv("BUTTON_EVENT")         or self._config["button"]["event"]
-        #bounce_millis = os.getenv("BUTTON_BOUNCE_MILLIS") or self._config["button"]["bounce_millis"]
-
         self.pin_clk       = os.getenv("BUTTON_CLK")           or self._config["button"]["PIN_CLK"]
         self.pin_dt        = os.getenv("BUTTON_DT")           or self._config["button"]["PIN_DT"]
         self.pin_button    = os.getenv("BUTTON_PIN")           or self._config["button"]["BUTTON_PIN"]
@@ -72,23 +68,14 @@ class App:
         # Initiales Auslesen des Pin_CLK
         self.PIN_CLK_LETZTER = GPIO.input(self.pin_clk)
 
-        # Variablen
-        #pin_clk = 0
-        #pin_dt = 0
-        #pin_button = 0
-        #PIN_CLK_AKTUELL = 0
-        #PIN_CLK_LETZTER = 0
-        # Counter = 0
-        Richtung = True
-
     def main(self):
         """
         Hauptverarbeitung des Skripts. Initialisiert den GPIO und startet eine
         Endlosschleife, um das Programm am Laufen zu halten.
         """
 
-        GPIO.add_event_detect(self.pin_clk, GPIO.BOTH, callback=self.ausgabeFunktion, bouncetime=50)
-        GPIO.add_event_detect(self.pin_button, GPIO.FALLING, callback=self.CounterReset, bouncetime=50)
+        GPIO.add_event_detect(self.pin_clk, GPIO.BOTH, callback=self._change_system_state, bouncetime=50)
+        GPIO.add_event_detect(self.pin_button, GPIO.FALLING, callback=self._deactivate_alert, bouncetime=50)
         try:
             while True:
                 time.sleep(10)
@@ -98,61 +85,43 @@ class App:
             self._logger.info("Räume GPIO auf")
             GPIO.cleanup()
     
-    def _on_button_pressed(self, channel):
-        """
-        Wert in der Datenbank ändern, wenn der Button gedrückt wurde.
-
-        THREADING: Diese Metheode läuft in einem separaten GPIO-Thread
-        """
-        time.sleep(1)
-
-        enabled = self._redis.get(REDIS_KEY_MEASUREMENT_ENABLED) != "0"
-
-        if enabled:
-            self._logger.info("Button wurde gedrückt: Stoppe Messung")
-            self._redis.set(REDIS_KEY_MEASUREMENT_ENABLED, "0")
-        else:
-            self._logger.info("Button wurde gedrückt: Reaktiviere Messung")
-            self._redis.set(REDIS_KEY_MEASUREMENT_ENABLED, "1")
     
-    def ausgabeFunktion(self, null):
-        # global Counter
+    def _change_system_state(self, null):
 
         PIN_CLK_AKTUELL = GPIO.input(self.pin_clk)
         
         if PIN_CLK_AKTUELL != self.PIN_CLK_LETZTER:
 
             if GPIO.input(self.pin_dt) != PIN_CLK_AKTUELL:
-                # self.Counter += 1
                 Richtung = True
             else:
                 Richtung = False
-                # Counter = self.Counter - 1
             
-            self._logger.info("Drehung erkannt: ")
             if Richtung:
-                self._logger.info("Drehrichtung: Im Uhrzeigersinn")
-                self._redis.set(REDIS_KEY_MEASUREMENT_ENABLED, "0")
+                self._logger.info("Alarmsystem deaktiviert")
+                self._redis.set(REDIS_ALERT_SYSTEM_ACTIVE, "0")
             else:
-                self._logger.info("Drehrichtung: Gegen den Uhrzeigersinn")
-                self._redis.set(REDIS_KEY_MEASUREMENT_ENABLED, "1")
+                self._logger.info("Alarmsystem aktiviert")
+                self._redis.set(REDIS_ALERT_SYSTEM_ACTIVE, "1")
 
-            # self._logger.info("------------------------------")
-        # self._logger.info("ausgabeFunktion")
-        # self._logger.info(PIN_CLK_AKTUELL)
-        # self._logger.info(self.PIN_CLK_LETZTER)
-        # self._logger.info(GPIO.input(self.pin_clk))
-        # self._logger.info(GPIO.input(self.pin_dt))
-        # self._logger.info(GPIO.input(self.pin_button))
+            
+    def _deactivate_alert(self, null):
+        if self._is_alert_enabled():
+            self._logger.info("Alarm deaktiviert!")
+            self._redis.set(REDIS_ALERT_ENABLED, "0")
+        else:
+            self._logger.info("Alarm bereits deaktiviert!")
+   
 
-    def CounterReset(self, null):
-    #    global Counter
-
-       self._logger.info("Alarm deaktivieren!")
-    #    self._logger.info("------------------------------")
-    #    Counter = 0
-    # Um einen Debounce direkt zu integrieren, werden die Funktionen zur Ausgabe mittels
-    # CallBack-Option vom GPIO Python Modul initialisiert
+    def _is_alert_enabled(self):
+        """Prüft, ob der Alarm bereits ausgelöst ist.
+        Returns:
+            bool: Wahr, wenn der Alarm bereits aktiv ist
+        """
+        if self._redis.get(REDIS_ALERT_ENABLED) == "1":
+            return True
+        else:
+            return False
     
 
 if __name__ == "__main__":
