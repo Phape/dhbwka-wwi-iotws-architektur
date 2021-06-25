44d85f0336030b83a68239b51f706a4a71fd5ba9
Philipp Mayer <philipp.mayer@phape.de>
2021-06-25 18:45:55 +0200

    add mqtt2mysql (currently not working)

---
+++ backend/receiver/mqtt2mysql.sh
@@ -0,0 +1,1 @@
+docker run -d -e GIT_REPOSITORY:"https://github.com/David-Lor/MQTT2MySQL.git" -e SQL_USER:root -e SQL_PASSWORD:example -e SQL_DATABASE:mqtt --name mqtt2mysql davidlor/python-autoclonable-app
